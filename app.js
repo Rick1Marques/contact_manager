8094171bd23ae801bd25482da08bf74879da25d7
Henrique Marques <henriquemarques@Henriques-Mac-mini.fritz.box>
2024-04-26 12:46:12 +0200

    set logged model user to req.user

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import path from "path";
 import dotenv from "dotenv";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import User from "./models/user.js";
 
 import authRoutes from "./routes/auth.js";
 
@@ -32,6 +33,20 @@ app.use(
     store: store,
   })
 );
+
+app.use(async (req, res, next) => {
+  try {
+    if (!req.session.user) {
+      return next();
+    }
+    const user = await User.findById(req.session.user._id);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
 
 app.use(authRoutes);
 
